46e76b02d4ae98f88fbf346a430e6f0455d0eb1e
Miclasher <14122006mg@gmail.com>
2025-10-17 23:17:53 +0300

    Add all node types

---
+++ ExcelClone.Domain/ExpressionLogic/AstNodes/FunctionCallNode.cs
@@ -0,0 +1,14 @@
+﻿namespace ExcelClone.Domain.ExpressionLogic.AstNodes;
+
+public class FunctionCallNode : AstNode
+{
+    public FunctionCallNode(string functionName, List<AstNode> arguments)
+    {
+        FunctionName = functionName;
+        Arguments = arguments;
+    }
+
+    public string FunctionName { get; }
+    public List<AstNode> Arguments { get; }
+
+}
