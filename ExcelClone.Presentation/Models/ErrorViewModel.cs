7b70d0b709fafd78fed61066f5cbb07accadcf23
Miclasher <14122006mg@gmail.com>
2025-10-17 20:55:51 +0300

    Add necessary projects

---
+++ ExcelClone.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExcelClone.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
