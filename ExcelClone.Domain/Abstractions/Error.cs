9ebaa5b27ead48eb9744f029e1b40ab304bdb9f7
Miclasher <14122006mg@gmail.com>
2025-10-17 22:16:07 +0300

    Simplify new()

--- ExcelClone.Domain/Abstractions/Error.cs
+++ ExcelClone.Domain/Abstractions/Error.cs
@@ -4,6 +4,6 @@ namespace ExcelClone.Domain.Abstractions;
 
 public sealed record Error(string Code, string Name)
 {
-    public static Error None() => new Error(string.Empty, string.Empty);
-    public static Error NullValue() => new Error("Error.NullValue", "Provided value was null");
+    public static Error None() => new(string.Empty, string.Empty);
+    public static Error NullValue() => new("Error.NullValue", "Provided value was null");
 };
