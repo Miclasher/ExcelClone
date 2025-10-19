0569ee3824a64bcba7308826a0b0dcaccdead00f
Miclasher <14122006mg@gmail.com>
2025-10-19 14:29:20 +0300

    Converted to file scoped namespace

--- ExcelClone.Tests/ParserTests.cs
+++ ExcelClone.Tests/ParserTests.cs
@@ -1,31 +1,28 @@
 using ExcelClone.Domain.ExpressionLogic;
 using ExcelClone.Domain.ExpressionLogic.AstNodes;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ExcelClone.Tests;
 
-namespace ExcelClone.Tests
+[TestClass]
+public class ParserTests
 {
-    [TestClass]
-    public class ParserTests
+    [TestMethod]
+    public void SimpleMultiplicationParserTest()
     {
-        [TestMethod]
-        public void SimpleMultiplicationParserTest()
-        {
-            var parser = new Parser();
+        var parser = new Parser();
             
-            var result = parser.Parse("=123 * 2");
+        var result = parser.Parse("=123 * 2");
             
-            Assert.IsInstanceOfType(result, typeof(BinaryOperationNode));
-            var binaryNode = (BinaryOperationNode)result;
-            Assert.AreEqual("*", binaryNode.Op);
+        Assert.IsInstanceOfType(result, typeof(BinaryOperationNode));
+        var binaryNode = (BinaryOperationNode)result;
+        Assert.AreEqual("*", binaryNode.Op);
             
-            Assert.IsInstanceOfType(binaryNode.Left, typeof(ValueNode));
-            Assert.IsInstanceOfType(binaryNode.Right, typeof(ValueNode));
+        Assert.IsInstanceOfType(binaryNode.Left, typeof(ValueNode));
+        Assert.IsInstanceOfType(binaryNode.Right, typeof(ValueNode));
             
-            var leftValue = ((ValueNode)binaryNode.Left).Value;
-            var rightValue = ((ValueNode)binaryNode.Right).Value;
+        var leftValue = ((ValueNode)binaryNode.Left).Value;
+        var rightValue = ((ValueNode)binaryNode.Right).Value;
             
-            Assert.AreEqual(123m, leftValue);
-            Assert.AreEqual(2m, rightValue);
-        }
+        Assert.AreEqual(123m, leftValue);
+        Assert.AreEqual(2m, rightValue);
     }
 }
